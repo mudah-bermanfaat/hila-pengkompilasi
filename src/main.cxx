e815a3e5b7d7bb4c4cfe24072b216b20cafb94b2
maszhar <fikrimustofa024@gmail.com>
2025-01-01 11:54:44 +0700

    fitur: memastikan ada argumen lokasi folder

--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,11 @@
-int main() {
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf("Kesalahan: argumen tidak sesuai!\n");
+        printf("Cara menggunakan: khila [lokasi folder kode sumber]\n");
+        printf("Kompilasi dibatalkan.\n");
+        return 1;
+    }
     return 0;
 }
